ff2435810333a1e5035d382e1a8b1c3595282d05
brendo <brendoja@gmail.com>
2021-04-12 18:16:05 -0300

    test fot the new class

---
+++ test.py
@@ -0,0 +1,21 @@
+import findInUrl as fd
+
+tag_name={
+    'root': 'li',
+    'apply_link': 'h2',
+    'published_date':'span',
+    'company_name': 'h3',
+    'skills':'span'}
+class_name={
+    'root': 'clearfix job-bx wht-shd-bx',
+    'apply_link': '',
+    'published_date':'sim-posted',
+    'company_name': 'joblist-comp-name',
+    'skills':'srp-skills'}
+
+finder = fd.findInUrl(
+    "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=",
+    tag_name,
+    class_name,
+    'Posted few days ago')
+print(finder.find())
